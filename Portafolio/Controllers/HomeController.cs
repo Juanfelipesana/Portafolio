ece88bace5b61f5753e5111ed8816b1b24cadfe0
Juan Felipe Sanabria Gonzalez <felipe_sanabria1998@hotmail.com>
2023-10-24 23:10:13 -0500

    inicial

---
+++ Portafolio/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portafolio.Models;
+using Portafolio.Servicios;
+using System.Diagnostics;
+
+namespace Portafolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly IRepositorioProyectos repositorioProyectos;
+
+        public HomeController(IRepositorioProyectos repositorioProyectos)
+        {
+            this.repositorioProyectos = repositorioProyectos;
+
+        }
+
+        public IActionResult Index()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList(); // crea una variable de proyectos y la llena con el método creado para los proyectos,
+                                                                                      // los lista tomando solo 3.
+            var modelo = new HomeIndexViewModel()
+            {
+                Proyectos = proyectos,
+            }; //crea un modelo de tipo homeindexvm al cual le asigna los proyectos
+            return View(modelo);
+        }
+        
+        public IActionResult Proyectos() 
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+
+            return View(proyectos);
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost] //Atributo, (Por defecto tienen HttpGet), estamos indicando que la acción siguiente se va a ejecutar cuando recibamos un HttpPost
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias() 
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Portafolio/Program.cs
@@ -0,0 +1,31 @@
+using Portafolio.Servicios;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IRepositorioProyectos, RepositorioProyectos>(); //Cuando una clase pida una instancia de Repositorio, se le envia IRepositorio
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute( //Ruteo convencional
+    name: "default", //valores por defecto
+    pattern: "{controller=Home}/{action=Index}/{id?}");// patron, primero el controlador,
+                                                       // luego la acción y por último un id opcional
+
+app.Run();
